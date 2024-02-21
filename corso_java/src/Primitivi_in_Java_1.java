71cdc30e6b2ddbeb27eb58cba2be818f44e91b8b
FusRoDahh <67054798+FusRoDahh@users.noreply.github.com>
2024-02-21 08:38:42 +0100

    Primi due esercizi

---
+++ corso_java/src/Primitivi_in_Java_1.java
@@ -0,0 +1,11 @@
+
+public class Primitivi_in_Java_1 {
+    public static void main(String[] args) {
+       int triangleBase = 10;
+       int triangleHeight = 15;
+
+       int triangleArea = triangleBase * triangleHeight / 2;
+
+       System.out.println("L'area del triangolo è uguale a: " + triangleArea);
+       }
+    }
---
+++ corso_java/src/Primitivi_in_Java_2.java
@@ -0,0 +1,8 @@
+public class Primitivi_in_Java_2 {
+    public static void main(String[] args) {
+        char firstChar = 'a';
+        char secondChar = 'b';
+
+        System.out.println("La somma dei due caratteri è: " + firstChar + secondChar);
+    }
+}
